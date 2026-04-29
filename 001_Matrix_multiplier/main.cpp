50e88ad9e83cc03786faf8893332bab87559fd8c
Marino Cobo <josemartincoboh@gmail.com>
2026-04-28 23:03:12 -0500

    Added Main and README to the project

---
+++ 001_Matrix_multiplier/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+
+
+void matrix_Generator
+(
+    int rows, 
+    int columns, 
+    std::vector<int>& r,
+    std::vector<std::vector<int>> c
+)
+{   
+    std::vector<int> rows;
+    std::vector<std::vector<int>> columns;
+}
+
+
+void matrix_Filler
+(
+    int rows,  
+    std::vector<int>& r,
+    std::vector<std::vector<int>> c
+)
+{
+    for(int i = 0; i < rows - 1; i++)
+    {
+        std::cout << "Enter row N°" << "
+    }
+
+}
+
+
+// 3 3 123 123 123 
+//2 3
